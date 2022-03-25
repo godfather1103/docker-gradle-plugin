f895b6f2407c503866258bbd36b978895a99c0e2
Jack Chu <chuchuanbao@gmail.com>
2022-03-25 22:45:23 +0800

    refactor(路径): 调整相关工程路径

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,5 @@ pluginManagement {
 
 include(":groovy-dsl-demo")
 include(":kotlin-dsl-demo")
+project(":groovy-dsl-demo").projectDir = file("example/groovy-dsl-demo")
+project(":kotlin-dsl-demo").projectDir = file("example/kotlin-dsl-demo")
